9bbe332a7fcdecef1d3f4b7ab816eb7093cab473
paraIncog <selgeandreas02@gmail.com>
2024-10-04 18:36:02 +0300

    Copy styles csnarr and end

--- narration/endings/end_0.js
+++ narration/endings/end_0.js
@@ -1,13 +1,20 @@
 function end_0() {
 	end0Done = true
 	mainContent.innerHTML = `
+
 	<div class="end-page">
-		<img class="cs-narr-pic" src="./img/endings/livonian_war_flag.png" alt="Ending 0" />
-		<div class="ty-msg">
-			Crusaders left Aestonian lands back to Riga. The command from God discarded, papal orders burnt, now the military has truly become the State.
+		<div class="end-img-container">
+			<img class="end-img" src="./img/endings/livonian_war_flag.png" alt="narrative pic" />
 		</div>
-		<div>
-			<button onclick="endsDoneCount()">Continue</button>
+		<div class="end-msg">
+
+			"Crusaders left Aestonian lands back to Riga. The command from God discarded, papal orders burnt, now the military has truly become the State."
+
+			<div class="end-cont-btn">
+				<button onclick="endsDoneCount()">
+					Continue
+				</button>
+			</div>
 		</div>
 	</div>
 	`;
